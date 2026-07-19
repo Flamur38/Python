0f6fd155116f1a2a854a918b677d459c00aa2450
Flamur38 <mulihtb@proton.me>
2026-07-19 20:31:24 +0200

    updated

---
+++ Automate_Boring_Stuff/loops/fiveTimes.py
@@ -0,0 +1,17 @@
+
+print('Hello!')
+for i in range(5):
+    print('On this iteration, i is set to ' + str(i))
+print('Goodbye!')
+
+
+print()
+
+
+# The same code writen in a "While loop"
+print('Hello!')
+i = 0
+while i < 5:
+    print('On this iteration, i is set to ' + str(i))
+    i = i + 1
+print('Goodbye!')
---
+++ Automate_Boring_Stuff/loops/test.py
@@ -0,0 +1,10 @@
+
+
+for i in range(0, 10, 2):
+    print(i)
+
+print()
+
+for i in range(5, -1, -1):
+    print(i)
+
